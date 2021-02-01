1c93a910148bafb0b28654e72981c1b7947b6fa9
Noimot <kikkyal@gmail.com>
2021-02-01 02:41:15 +0100

    distance converter app completed

---
+++ converter.js
@@ -0,0 +1,56 @@
+let input_value = document.querySelector("#input_value");
+let calculatorAnswer = document.querySelector("#calculatorAnswer");
+let convertFrom = document.querySelector("#convertFrom");
+let convertTo = document.querySelector("#convertTo");
+let calculateBtn = document.querySelector("#calculateBtn");
+let clearBtn = document.querySelector("#clearBtn");
+
+
+
+let converter = new Converter()
+
+calculateBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (Math.sign(input_value.value) == -1)  return
+    
+    if (input_value.value) {
+        console.log(converter)
+        console.log(converter.convertKmToM(input_value.value))
+        calculateType(`${convertFrom.value}/${convertTo.value}`)
+    }
+ 
+})
+
+function calculateType(type) {
+    console.log(type)
+    switch (type) {
+        case "kilometer/meter":
+            calculatorAnswer.innerText = `convert kilometer to meter: ${converter.convertKmToM(input_value.value)}`
+            break;
+        case 'meter/kilometer':
+            calculatorAnswer.innerText = `convert kilometer to meter: ${converter.convertMToKm(input_value.value)}`
+            break;
+        case 'miles/kilometer':
+            calculatorAnswer.innerText = `convert kilometer to meter: ${converter.convertMilesToKm(input_value.value)}`
+            break;
+        case 'kilometer/miles':
+            calculatorAnswer.innerText = `convert kilometer to meter: ${converter.convertKmToMiles(input_value.value)}`
+            break;
+        case 'centimeter/meter':
+            calculatorAnswer.innerText = `convert kilometer to meter: ${converter.convertCmToM(input_value.value)}`
+            break;
+        case 'meter/centimeter':
+            calculatorAnswer.innerText = `convert kilometer to meter: ${converter.convertMToCm(input_value.value)}`
+            break;
+        default:
+
+    }
+}
+
+
+
+clearBtn.addEventListener("click", function () {
+    if (input_value.value == "")
+    converter.clearBtn(input_value.value);
+})
---
+++ distance_converter_lib.js
@@ -0,0 +1,37 @@
+
+function Converter() {
+
+}
+
+Converter.prototype.convertKmToM = function (number) {
+    return number * 1000;
+}
+
+Converter.prototype.convertMToKm = function (number) {
+    return number / 1000;
+}
+
+Converter.prototype.convertMilesToKm = function (number) {
+    return number * 1.609344;
+}
+
+
+Converter.prototype.convertKmToMiles = function (number) {
+    return number / 1.609344;
+}
+
+
+Converter.prototype.convertCmToM = function (number) {
+    return number / 100;
+}
+
+
+Converter.prototype.convertMToCm = function (number) {
+    return number * 100;
+}
+
+Converter.prototype.clearBtn = function (number) {
+   return number;
+}
+
+
